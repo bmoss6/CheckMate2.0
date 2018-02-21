24f9654b8448a8cc1b4459e0514bbc6df4fe2006
Tanner Perdue <tannerlp@byu.net>
2018-02-21 10:42:41 -0700

    broke apart board class a little more and wrote a todo.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # CheckMate2.0
-New Checkmate Repo cause old was was broke. 
+
+### TODO
+- Get the board class filled out and added to the robot. (robot class)
+- Separate out peice class for other classes to use 
+- With each move update Pieces on board (board class)
+- Check for collision with each move (board class)
+- Add collision routine when a collision happens (robot/board class/collision board class?)
+- Add a routine that will check the GPIO pins after each move (gpio/board class)
+- Create a function for the invert moves of the other robot (position class)
+- Figure out how to auto detect which ports each of the robots are using 
+- Think of a simple interface that reset the robot when needed
+	- Piece of hardware that will trigger an interrupt on a RPI to reset all of the state
--- board.py
+++ board.py
@@ -1,28 +1,32 @@
-class board:
+#! /usr/bin/python3
+
+from piece import Piece
+
+class Board:
 
     def __init__(self, width=8, height=8):
         self.name = "8x8 Chess Board"
         self.width, self.height = width, height
         self.board = [[] for x in range (0,self.height)]
 
-            ##TODO Initialize starting board pieces for 8x8 grid.
+            ##TODO Initialize starting board Pieces for 8x8 grid.
 
 
     def construct_8x8_board(self):
 
         first_line = []
         blank_rows =[]
-        first_line.append(piece("rook","black"))
-        first_line.append(piece("knight", "black"))
-        first_line.append(piece("bishop", "black"))
-        first_line.append(piece("queen", "black"))
-        first_line.append(piece("king", "black"))
-        first_line.append(piece("bishop", "black"))
-        first_line.append(piece("knight", "black"))
-        first_line.append(piece("rook", "black"))
+        first_line.append(Piece("rook","black"))
+        first_line.append(Piece("knight", "black"))
+        first_line.append(Piece("bishop", "black"))
+        first_line.append(Piece("queen", "black"))
+        first_line.append(Piece("king", "black"))
+        first_line.append(Piece("bishop", "black"))
+        first_line.append(Piece("knight", "black"))
+        first_line.append(Piece("rook", "black"))
         second_line = []
         for x in range(0,self.width):
-            second_line.append(piece("pawn","black"))
+            second_line.append(Piece("pawn","black"))
         self.board[0] = first_line
         self.board[1] = second_line
         self.board[6] = second_line
@@ -31,7 +35,7 @@ class board:
             self.board[6][x].color = "white"
             self.board[7][x].color = "white"
         for x in range(0,self.width):
-            blank_rows.append(piece())
+            blank_rows.append(Piece())
         for x in range(2, 6):
             self.board[x] = blank_rows
 
@@ -45,30 +49,7 @@ class board:
 #   [null,  null,  null,   null,  null,  null,  null,  null]
 #   [pawn,  pawn,  pawn,   pawn,  pawn,  pawn,  pawn,  pawn]
 #   [ rook, knight, bishop, queen, king, bishop, knight, rook ]
-class piece:
-    def __init__(self, name="null", color="null"):
-        self.name = name
-        self.color = color
 
 
 
-def test():
-    testboard = board()
-    testboard.construct_8x8_board()
-    for x in range(0,len(testboard.board)):
-        for y in range(0,testboard.width):
-            print("||" +str(x)+str(y)+" " + testboard.board[x][y].color + " " + testboard.board[x][y].name +"||"),
-        print("------------------------------------")
-    i =0
-    j =0
 
-   # for x in testboard.board:
-    #    for y in testboard.board[i]:
-     #       print (str(i) + str(j) + "\n" + testboard.board[i][j].name +" "+ testboard.board[i][j].color)
-      #      j= j+1
-       # i = i+1
-        #print("--------------------------------------------------------")
-
-
-if __name__ == "__main__":
-    test()
