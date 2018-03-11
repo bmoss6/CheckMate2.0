770cfa81f05529f60372fc4ed8ed08b08914d059
bmoss6 <bmoss6@DESKTOP-LTSTBHP.localdomain>
2018-03-11 11:10:44 -0600

    integrated Map with GPIO class

--- gpio.py
+++ gpio.py
@@ -5,12 +5,23 @@ import itertools
 
 class GPIOBOARD():
     def __init__(self):
-        self.gpioboard = [[] for x in range (0,8)]
         self.Mux1= {'A': 24, 'B': 23, 'C': 18, 'D': 15, 'W': 14}
         self.Mux2= {'A': 16, 'B': 12, 'C': 7, 'D': 8, 'W': 25}
         self.Mux3= {'A': 4, 'B': 3, 'C': 2, 'D': 21, 'W': 20}
         self.Mux4= {'A': 19, 'B': 10, 'C': 22, 'D': 27, 'W': 17}
         self.MuxList = [self.Mux1, self.Mux2, self.Mux3, self.Mux4]
+        self.gpioboard=[ [0 for i in range(8)] for x in range(8)]
+
+        self.MuxToCoordinateMap = {"1.0":"7,3","1.1":"7,1","1.2":"7,2","1.3":"7,0","1.4":"5,3","1.5":"5,1","1.6":"5,2","1.7":"5,0","1.8":"6,3","1.9":"6,1","1.10":"6,2","1.11":"6,0","1.12":"4,3","1.13":"4,1","1.14":"4,2","1.15":"4,0","2.0":"7,7","2.1":"7,5","2.2":"7,6","2.3":"7,4","2.4":"5,7","2.5":"5,5","2.6":"5,6","2.7":"5,4","2.8":"6,7","2.9":"6,5","2.10":"6,6","2.11":"6,4","2.12":"4,7","2.13":"4,5","2.14":"4,6","2.15":"4,4","3.0":"3,3","3.1":"3,1","3.2":"3,2","3.3":"3,0","3.4":"1,3","3.5":"1,1","3.6":"1,2","3.7":"1,0","3.8":"2,3","3.9":"2,1","3.10":"2,2","3.11":"2,0","3.12":"0,3","3.13":"0,1","3.14":"0,2","3.15":"0,0","4.0":"3,7","4.1":"3,5","4.2":"3,6","4.3":"3,4","4.4":"1,7","4.5":"1,5","4.6":"1,6","4.7":"1,4","4.8":"2,7","4.9":"2,5","4.10":"2,6","4.11":"2,4","4.12":"0,7","4.13":"0,5","4.14":"0,6","4.15":"0,4"}
+
+    def updateboard(self,muxnumber,pinvalue,outputvalue):
+        MuxPinPosition = "{:d}.{:d}".format(muxnumber,pinvalue)
+        BoardCoordinates = test.get(MuxPinPosition)
+        BoardCoordinates = testobject.split(",")
+        x = BoardCoordinates[0]
+        y = BoardCoordinates[1]
+        self.gpioboard[int(x)][int(y)] = outputvalue
+ 
 
 
     def setup(self):
@@ -29,7 +40,7 @@ class GPIOBOARD():
         GPIO.output(MuxMap.get('C'), inputs[2])
         GPIO.output(MuxMap.get('D'), inputs[3])
         return GPIO.input(MuxMap.get('W'))
-    def boardcheck1(self,muxnumber=0):
+    def boardcheck(self,muxnumber=0):
         x = 0
         y = 0
         i = 1
@@ -41,8 +52,10 @@ class GPIOBOARD():
                 pin_values = list(map(int, pin_values))
                 if muxnumber is not None:
                     if i==muxnumber:
+                        self.updateboard(i,num,self.checkmux(mux,pin_values))
                         print ('{0} :  {1}'.format(str(pin_values),self.checkmux(mux, pin_values)))
                 else:
+                        self.updateboard(i,num,self.checkmux(mux,pin_values))
                     print ('{0} :  {1}'.format(str(pin_values),self.checkmux(mux, pin_values)))
             i= i+1
 
@@ -69,5 +82,5 @@ if __name__=='__main__':
     testgpio.setup()
     while(1):
         testgpio.boardcheck1(args.muxnumber)
-        print ("HK:KLKLJ:++++++++++++++++++++++++++++++++++++++++++")
+        print ("++++++++++++++++++++++++++++++++++++++++++")
         sleep(2)
